8ce3ae14feb25533f7affa30e83514076bb5f1d0
951237 <951237.gmail.com>
2021-01-06 00:56:28 +0900

    Doc : Create - 버전관리 시작 - 파일 업로드 구현 해결하기

---
+++ app.py
@@ -0,0 +1,17 @@
+'''
+파일 업로드 참고 : https://niceman.tistory.com/150
+'''
+
+from flask import Flask, request, render_template
+from PyPDF2 import PdfFileMerger, PdfFileReader, PdfFileWriter
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'GET':
+        return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host = '127.0.0.1', port=5000)
