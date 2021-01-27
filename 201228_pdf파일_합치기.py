f8d2397872d2b7dde217479e6b87b8de547bcda4
951237 <951237.gmail.com>
2021-01-28 02:32:17 +0900

    Doc : Update - 변수이름 수정 / 내가 이해하기 쉬운 변수로

--- 201228_pdf파일_합치기.py
+++ 201228_pdf파일_합치기.py
@@ -12,17 +12,17 @@ file1 = PdfFileReader(open(file_1, "rb"))
 file2 = PdfFileReader(open(file_2, "rb"))
 
 # pdf writer
-output = PdfFileWriter()
+pdf_writer = PdfFileWriter()
 
 # pdf파일의 총 페이지
 page_num = file1.getNumPages()
 
 # 두개의 파일 번갈아가며 합치기
 for i in range(page_num):
-    output.addPage(file1.getPage(0+i))
-    output.addPage(file2.getPage(((page_num-1))-i))
+    pdf_writer.addPage(file1.getPage(0+i))
+    pdf_writer.addPage(file2.getPage((page_num-1))-i)
 
-outputStream = open(result, "wb")
+result_file = open(result, "wb")
 
-output.write(outputStream)
-outputStream.close()
+pdf_writer.write(result_file)
+result_file.close()
